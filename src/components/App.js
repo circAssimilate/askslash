3a48cd89caf86acc3cf56502582237559ac424ef
circAssimilate <dehammond@me.com>
2017-01-15 17:03:38 -0800

    Updated using require and react dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import '../assets/stylesheets/base.scss';
-import React, { Component } from 'react';
+require('../assets/stylesheets/base.scss');
+var React = require('react');
 
-class App extends Component {
-  render() {
+var App = React.createClass({
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
+
+  render: function () {
     return(
       <h1>Hello, {this.props.name}!</h1>
     )
   }
-};
+});
 
-export default App;
+module.exports = App;
+
+// Original
+// import React, { Component } from 'react';
+//
+// class App extends Component {
+//   render() {
+//     return(
+//       <h1>Hello, {this.props.name}!</h1>
+//     )
+//   }
+// };
+//
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
 
-render(<App name='World'/>, document.getElementById('root'));
+var Parent = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Derek's Changes</h1>
+        <App name=''/>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('root')
+);
+
+// Original File
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './components/App';
+//
+// render(<App name='World'/>, document.getElementById('root'));
